cbd66ee83fb7b890e28d461f3194384bf09f71a7
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-05-22 23:59:33 -0400

    feat: add third string

--- simple-programs/strings.c
+++ simple-programs/strings.c
@@ -1,9 +1,17 @@
-#include <stdlib.h>
 #include <strings.h>
+#include <stdio.h>
 
 int main(void)
 {
-  char *example_string = "Hello World\n";
+	/* print out the same string 2 times */
+	char *example_string = "Hello World\n";
+	printf(example_string);
+	printf("%s", example_string);
 
-  return 0;
+	/* create a third string based on the first one
+	 * but append "Hi !\n" at the end of the string */
+	char other_string[24];	/* create an array of 24 characters */ 
+	snprintf(other_string, sizeof(other_string), "%sHi !\n", example_string);
+	printf("%s", other_string);
+	return 0;
 }
