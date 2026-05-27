b50e8a46273d5858357eb64753c9af9edb94d304
ImAwsumm <ImAwsumm@users.noreply.github.com>
2026-05-26 22:54:29 -0400

    refactor: improve argument logic
    add a double condition

--- command-parsing/square.c
+++ command-parsing/square.c
@@ -8,6 +8,7 @@ void invalid_cmd_message(void);
 
 int main(int argc, char *argv[])
 {
+	bool no_args = false;
 	int base_args_n = 1;
 	bool valid_args = false;
 
@@ -17,35 +18,27 @@ int main(int argc, char *argv[])
 	{
 		if (argc > 1)
 		{
-			if (argc > 2)
+			if ((argc > 2) && (argc == 3))
 			{
-				if (argc == 3)
+				char *endptr;
+				int index = base_args_n;
+				x = strtol(argv[index], &endptr, 10);
+				if (!(*endptr == '\0'))
 				{
-					char *endptr;
-					int index = base_args_n;
-					x = strtol(argv[index], &endptr, 10);
-					if (!(*endptr == '\0'))
-					{
-						invalid_cmd_message();
-						break;
-					}
-					index++;
-					y = strtol(argv[index], &endptr, 10);
-					if (!(*endptr == '\0'))
-					{
-						invalid_cmd_message();
-						break;
-					}
-					else
-					{
-						/* all variables taken from the user are valid */
-						valid_args = true;
-						break;
-					}
+					invalid_cmd_message();
+					break;
+				}
+				index++;
+				y = strtol(argv[index], &endptr, 10);
+				if (!(*endptr == '\0'))
+				{
+					invalid_cmd_message();
+					break;
 				}
 				else
 				{
-					invalid_cmd_message();
+					/* all variables taken from the user are valid */
+					valid_args = true;
 					break;
 				}
 			}
@@ -56,6 +49,15 @@ int main(int argc, char *argv[])
 				break;
 			}
 		}
+		else
+		{
+			no_args = true;
+		}
+	}
+
+	if (!no_args)
+	{
+		return 0;
 	}
 
 	char *word_to_print = "Hi";
@@ -70,9 +72,9 @@ int main(int argc, char *argv[])
 
 	/* initialize the word buffer
 	 * then write the word to it while making sure we aren't overflowing the buffer */
-    char word[max_word_size];
+	char word[max_word_size];
 
-    snprintf(word, (size_t)buf_size_word, "%s", word_to_print);
+    	snprintf(word, (size_t)buf_size_word, "%s", word_to_print);
 
 	/* get the user input for the x and y axis
 	 * we are doing "if (!valid_args)" because valid args is only true if the input is valid 
@@ -80,31 +82,31 @@ int main(int argc, char *argv[])
 	if (!valid_args)
 	{
 		printf("Type the X axis: ");
-    	scanf("%ld", &x);
+		scanf("%ld", &x);
 
-    	while (getchar() != '\n'); /* clear the stdin input buffer */
+		while (getchar() != '\n'); /* clear the stdin input buffer */
 
-    	printf("Type the y axis: ");
-    	scanf("%ld", &y);
+    		printf("Type the y axis: ");
+    		scanf("%ld", &y);
 	}
 
 	/* errors out if the user is trying to print nothing */
-    if (!(x > 0) || !(y > 0))
-    {
+	if (!(x > 0) || !(y > 0))
+	{
 		printf("No characters to print out\n");
 		exit(1);
-    }
+	}
+	
+	printf("\n"); /* print newline for clarity */
 
-    printf("\n"); /* print newline for clarity */
-
-    for (int i = 0; i < y; i++)
-    {
+	for (int i = 0; i < y; i++)
+    	{
 		for (int j = 0; j < x; j++)
 		{
-		    printf("%s ", word);
+			printf("%s ", word);
 		}
 		printf("\n"); /* print newline because we did a full line */
-    }
+	}
 	return 0;
 }
 
