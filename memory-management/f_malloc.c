f512718048f4c6e5f210f7277e45967f9a9a9b52
ImAwsumm <ImAwsumm@users.noreply.github.com>
2026-05-28 22:57:15 -0400

    feat: make use of number array
    print numbers from the argv command

--- memory-management/f_malloc.c
+++ memory-management/f_malloc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define test_number 123
+#define test_number (123)
 
 int main(int argc, char *argv[])
 {
@@ -12,16 +12,28 @@ int main(int argc, char *argv[])
 		auto long int number;
 
 		number = strtol(str, &endptr, 10);
+
 		if (!(*endptr != '\0'))
 		{
 			printf("number is: %ld\n", number);
 		}
 		auto int array_elements = argc - 1;
 		auto int numbers[array_elements];
+
+
+
+		for (int i = 0; i < array_elements; i++)
+		{
+			int num = i + 1;
+			char *end_ptr;
+			int command_number = (int)strtol(argv[num], &end_ptr, 10);
+			numbers[i] = command_number;
+			printf("%d\n", numbers[i]);
+		}
 	}
 
 	auto int y;
-	auto int *x = malloc(4);
+	auto int *x = malloc(4); /* size of the integer data type */
 
 	x = &y;
 	y = test_number;
