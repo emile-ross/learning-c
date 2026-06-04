2c9668eab42a0d9ac6c20291403d71e94c5ae9c8
emile-ross <emileross@tutanota.de>
2026-06-03 23:52:52 -0400

    build: fix invalid naming in makefile
    also fix indenting in taxes.c

--- math/taxes.c
+++ math/taxes.c
@@ -2,28 +2,28 @@
 
 int main(void)
 {
-    float u_input;
-    float u_input_tax_rate;
-
-    printf("Type your expense: ");
-    scanf("%f", &u_input);
-
-    while (getchar() != '\n');
-
-    printf("Type your tax rate in %%: ");
-    scanf("%f", &u_input_tax_rate);
-
-    if (!(u_input >= 0))
-    {
-		printf("Invalid input\n");
-		return 1;
-    }
-
-    float tax_rate = u_input_tax_rate / 100 + 1;
-    float total = tax_rate * (float)u_input;
-
-    printf("%f\n", u_input);
-
-    printf("%f\n", total);
-    return 0;
+	float u_input;
+	float u_input_tax_rate;
+	
+	printf("Type your expense: ");
+	scanf("%f", &u_input);
+	
+	while (getchar() != '\n');
+	
+	printf("Type your tax rate in %%: ");
+	scanf("%f", &u_input_tax_rate);
+	
+	if (!(u_input >= 0))
+	{
+	    	printf("Invalid input\n");
+	    	return 1;
+	}
+	
+	float tax_rate = u_input_tax_rate / 100 + 1;
+	float total = tax_rate * (float)u_input;
+	
+	printf("%f\n", u_input);
+	
+	printf("%f\n", total);
+	return 0;
 }
