96fce5f19d3e3903b28a0584bbf1f16e1c5da226
Emile Ross <153707128+emile-ross@users.noreply.github.com>
2026-07-31 12:46:40 +0200

    feat: add error checking
    Adds error checking in main()

--- word_invert/main.c
+++ word_invert/main.c
@@ -5,6 +5,10 @@ int main(void)
 {
   size_t input_max_len = 255;
   char *input_buffer = malloc(input_max_len);
-	fgets(input_buffer, (int)input_max_len, stdin);
+	if (fgets(input_buffer, (int)input_max_len, stdin) == NULL)
+	{
+    fprintf(stderr, "Failed to get input from fgets()\n");
+    return 1;
+	}
   return 0;
 }
