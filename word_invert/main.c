bef54e888c3bc21df93dd566fdde4968773b0023
Emile Ross <153707128+emile-ross@users.noreply.github.com>
2026-08-04 22:58:46 +0200

    style: fix indenting (tabs)
    replace tabs with spaces

--- word_invert/main.c
+++ word_invert/main.c
@@ -6,28 +6,28 @@ int display_word(char *word, size_t len);
 
 int main(void)
 {
-  size_t input_max_len = 255;
-  char *input_buffer = malloc(input_max_len);
+	size_t input_max_len = 255;
+	char *input_buffer = malloc(input_max_len);
 	if (fgets(input_buffer, (int)input_max_len, stdin) == NULL)
 	{
-    fprintf(stderr, "Failed to get input from fgets()\n");
-    free(input_buffer);
-    return 1;
+		fprintf(stderr, "Failed to get input from fgets()\n");
+		free(input_buffer);
+		return 1;
 	}
-
-  /* take in account the null terminator **///
-  size_t user_input_len = 1 + strlen(input_buffer);
-
-  /* allocate memory for the user stringg
-  * this is a different buffer because it doesn'tt
-  * need the full size the first buffer had *///
-  char *user_input = malloc(user_input_len);
-  snprintf(user_input, user_input_len, "%s", input_buffer);
-  free(input_buffer);
-
-
-  free(user_input);
-  return 0;
+	
+	/* take in account the null terminator **///
+	size_t user_input_len = 1 + strlen(input_buffer);
+	
+	/* allocate memory for the user stringg
+	* this is a different buffer because it doesn'tt
+	* need the full size the first buffer had *///
+	char *user_input = malloc(user_input_len);
+	snprintf(user_input, user_input_len, "%s", input_buffer);
+	free(input_buffer);
+	
+	
+	free(user_input);
+	return 0;
 }
 
 int game(char *word, size_t len)
